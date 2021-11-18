afe604ad3500b8a1a7bde8e673c43dda7e9e4075
Braulio Barros <braulio.barros@engenharia.ufjf.br>
2021-11-18 16:58:20 -0300

    Unindo pistas, camera e carinho

--- src/exampleGroup.js
+++ src/exampleGroup.js
@@ -50,13 +50,10 @@ var group = new THREE.Group();
 var axesHelper = new THREE.AxesHelper( 12 );
 
 // Set the parts of the pseudo-car
-var body = createCylinder(2.0, 2.7, 12.0, 20, 20, false, 1);
+var body = createCylinder(2.0, 2.7, 10.0, 20, 20, false, 1);
   body.rotateX(degreesToRadians(90));
-  body.position.set(0.0, 0.5, 0.0);
+  body.position.set(0.0, 0.5, 0.0)
 
-var front = createRectangle();
-  front.position.set();
-//eixos carrinho
 var axis1 = createCylinder(0.3, 0.3, 7.0, 10, 10, false);
   axis1.rotateZ(degreesToRadians(90));
   axis1.position.set(0.0, -1.0, 4.0);
@@ -77,18 +74,6 @@ var roda3 = createTorus(1.0, 0.3, 20, 20, Math.PI * 2);
 var roda4 = createTorus(1.0, 0.3, 20, 20, Math.PI * 2);
   roda4.position.set(-3.5, -1.0, -4.0);
 
-//var lateralDir = createRectangle(1.5, 0.5, 3.0);
-//lateralDir.position.set(-3.0, 1.0, 2.0);
-
-//var lateralEsq = createRectangle(1.5, 0.5, 3.0);
-//lateralEsq.position.set(3.0, 1.0, 2.0);
-
-var atras = createRectangle(10.5, 0.5, 2.0);
-atras.position.set(0.0, 1.5, -6.9);
-
-var frente = createRectangle(7.5, 0.5, 2.0);
-frente.position.set(0.0, 0.5, 6.9);
-
 // Add objects to the group
 group.add( axesHelper );
 group.add( body );
@@ -98,10 +83,6 @@ group.add( roda1 );
 group.add( roda2 );
 group.add( roda3 );
 group.add( roda4 );
-//group.add( lateralDir);
-//group.add( lateralEsq);
-group.add( frente);
-group.add( atras);
 
 // Add group to the scene
 scene.add(group);
@@ -112,14 +93,15 @@ group.rotateY(degreesToRadians(-90));
 
 render();
 
+
 function createCylinder(radiusTop, radiusBottom, height, radialSegments, heightSegments, openEnded, color)
 {
   var geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments, heightSegments, openEnded);
   var material;
   if(!color)
-    material = new THREE.MeshPhongMaterial({color:"rgb(125,0,0)"});
+    material = new THREE.MeshPhongMaterial({color:"rgb(255,0,0)"});
   else
-    material = new THREE.MeshPhongMaterial({color:"rgb(0,0,0)"});
+    material = new THREE.MeshPhongMaterial({color:"rgb(230,120,50)"});
   var object = new THREE.Mesh(geometry, material);
     object.castShadow = true;
   return object;
@@ -128,7 +110,7 @@ function createCylinder(radiusTop, radiusBottom, height, radialSegments, heightS
 function createTorus(radius, tube, radialSegments, tubularSegments, arc)
 {
   var geometry = new THREE.TorusGeometry(radius, tube, radialSegments, tubularSegments, arc);
-  var material = new THREE.MeshPhongMaterial({color:"rgb(255,255,255)"});
+  var material = new THREE.MeshPhongMaterial({color:"rgb(125,125,125)"});
   var object = new THREE.Mesh(geometry, material);
     object.castShadow = true;
     object.rotateY(degreesToRadians(90));
@@ -138,18 +120,9 @@ function createTorus(radius, tube, radialSegments, tubularSegments, arc)
 function createSphere(radius, widthSegments, heightSegments)
 {
   var geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments, 0, Math.PI * 2, 0, Math.PI);
-  var material = new THREE.MeshBasicMaterial({color:"rgb(10,10,10)"});
+  var material = new THREE.MeshBasicMaterial({color:"rgb(255,255,50)"});
   var object = new THREE.Mesh(geometry, material);
     object.castShadow = true;
-  return object;
-}
-
-function createRectangle(width, height, depth)
-{
-  var geometry = new THREE.BoxGeometry(width, height, depth);
-  var material = new THREE.MeshPhongMaterial({color:"rgb(12, 96, 126)"});
-  var object = new THREE.Mesh(geometry,material);
-  object.castShadow = true;
   return object;
 }
 
@@ -172,7 +145,7 @@ function showInformation()
 {
   // Use this to show information onscreen
   var controls = new InfoBox();
-    controls.add("Carrinho");
+    controls.add("Group Example");
     controls.addParagraph();
     controls.add("Use mouse to rotate/pan/zoom the camera");
     controls.add("Up / Arrow to walk");
