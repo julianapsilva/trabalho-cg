b3f01564d2b81a34365f0ad7e9666691cd57bece
julianapsilva <julianadepaula001@gmail.com>
2021-11-26 19:59:56 -0300

    modularizando o codigo

--- works/trabalho1/index.js
+++ works/trabalho1/index.js
@@ -4,6 +4,8 @@ import { TrackballControls } from '../../build/jsm/controls/TrackballControls.js
 import KeyboardState from '../../libs/util/KeyboardState.js';
 import mountCar from './car/mountCar.js';
 import handleCamera from './camera/handleCamera.js';
+import createPlane from './pistas/plane.js';
+import mudaPista from './pistas/pistas.js';
 import {
     initRenderer,
     InfoBox,
@@ -21,14 +23,11 @@ var scene = new THREE.Scene();    // Create main scene
 var stats = new Stats();          // To show FPS information
 var renderer = initRenderer();    // View function in util/utils
 renderer.setClearColor("rgb(30, 30, 40)");
+
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.lookAt(0, 0, 0);
 camera.position.set(5, 15, 50);
 camera.up.set(0, 1, 0);
-var upVec = new THREE.Vector3(0, 1, 0);
-// camera.up.set(upVec);
-
-
 
 var newScene = new THREE.Scene();    // Create main scene
 var inspectionCamera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -38,8 +37,6 @@ newScene.add(car)
 
 inspectionCamera.lookAt(0, 0, 0)
 inspectionCamera.position.set(47, 0, 0)
-
-
 
 
 var clock = new THREE.Clock();
@@ -59,95 +56,9 @@ function createSphere(radius, widthSegments, heightSegments) {
     return object;
 }
 
-
-var planeGeometry = new THREE.PlaneGeometry(360, 360);
-// planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
-var planeMaterial = new THREE.MeshBasicMaterial({
-    color: 'rgb(211, 154, 8)',
-    side: THREE.DoubleSide,
-});
-var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.rotateX(degreesToRadians(-90));
-// add the plane to the scene
-scene.add(plane);
+createPlane(scene);
 // Show text information onscreen
 showInformation();
-
-var pista1_obj = [];
-var pista2_obj = [];
-
-var isPista = 0; // 1 >> Modo pista 1 || 2 >> Modo pista 2
-function blocoComum(x, z){
-  var cubeGeometry = new THREE.BoxGeometry(50, 0.3, 50);
-  var cubeMaterial = new THREE.MeshBasicMaterial({ color: 'rgb(50,50,50)' });
-  var cube;
-
-  cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  cube.position.set(x, 0.15, z);
-  scene.add(cube);
-}
-function blocoInicio(x, z){
-  var cubeGeometry = new THREE.BoxGeometry(50, 0.4, 50);
-  var cubeMaterial = new THREE.MeshBasicMaterial({ color: 'rgb(255, 0, 0)', });
-  var cube;
-
-  cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  cube.position.set(x, 0.15, z);
-
-  scene.add(cube);
-}
-function mudarPista() {
-  //Pista 1
-  if (isPista == 1) {
-    for (let i = -150; i < 151; i += 30) {
-      for (let j = -150; j < 151; j += 30) {
-        if (i == -150 || i == 150 || j == -150 || j == 150) {
-          blocoComum(i,j);
-        }
-        if (i == -30 && j == -150) {
-          blocoInicio(i,j);
-        }
-      }
-    }
-  }
-  //Pista 2
-  if (isPista == 2) {
-    for (let i = -150; i < 151; i += 30) {
-      for (let j = -150; j < 151; j += 30) {
-        if (i == -150) // reta 1
-        {
-          blocoComum(i,j);
-        }
-        if (j == 150 && i <= 0) // reta 2
-        {
-          blocoComum(i,j);
-        }
-        if (i == 0 && j >= 0) // reta 3
-        {
-          blocoComum(i,j);
-        }
-        if (j == 0 && i >= 0) // reta 4
-        {
-          blocoComum(i,j);
-        }
-        if (i == 150 && j <= 0) // reta 5
-        {
-          blocoComum(i,j);
-        }
-        if (j == -150) // reta 6
-        {
-          blocoComum(i,j);
-        }
-        if (i == -30 && j == -150) {
-          blocoInicio(i,j);
-        }
-      }
-    }
-    scene.children[4].userData.teste = 'um teste'
-    // scene.children.map(child => console.table(child.position))
-  }
-}
-
 
 // To use the keyboard
 var keyboard = new KeyboardState();
@@ -158,7 +69,6 @@ var trackballControls = new TrackballControls(inspectionCamera, renderer.domElem
 var group = mountCar()
 scene.add(group)
 camera.position.set(97, 19, -91)
-console.log(group);
 render()
 
 var pressionadoLeft = false;
@@ -210,17 +120,13 @@ function keyboardUpdate() {
         else
             scene.visible = true
     }
-    if (keyboard.pressed("6")) {
-        scene.visible = true
-    }
+
     // Muda o tipo de pista
     if (keyboard.down("7")) {
-        isPista = 1;
-        mudarPista();
+        mudaPista(scene, 1);
     }
     if (keyboard.down("8")) {
-        isPista = 2;
-        mudarPista();
+        mudaPista(scene, 2);
     }
     // Guarda a mudança de estado das teclas
     if (keyboard.up("left")) {
@@ -270,18 +176,6 @@ function verifyPosition() {
     //   position = 3;
     // }
 
-}
-
-function colision() {
-    //   scene.children.map((child) => {
-    //     if (child.type == 'Mesh' && child.id == 70) {
-    //     console.log(child.id)
-    const firstBB = new THREE.Box3().setFromObject(groundPlane)
-    const secondBB = new THREE.Box3().setFromObject(roda1);
-    var collision = firstBB.intersectsBox(secondBB);
-    console.log(collision, firstBB, secondBB)
-    //   }
-    // })
 }
  
 function showInformation() {
---
+++ works/trabalho1/pistas/pistas.js
@@ -0,0 +1,75 @@
+import * as THREE from '../../../build/three.module.js';
+
+
+export default function mudaPista(scene, isPista) {
+    //Pista 1
+    if (isPista == 1) {
+        for (let i = -150; i < 151; i += 30) {
+            for (let j = -150; j < 151; j += 30) {
+                if (i == -150 || i == 150 || j == -150 || j == 150) {
+                    blocoComum(i, j);
+                }
+                if (i == -30 && j == -150) {
+                    blocoInicio(i, j);
+                }
+            }
+        }
+    }
+    //Pista 2
+    if (isPista == 2) {
+        for (let i = -150; i < 151; i += 30) {
+            for (let j = -150; j < 151; j += 30) {
+                if (i == -150) // reta 1
+                {
+                    blocoComum(i, j);
+                }
+                if (j == 150 && i <= 0) // reta 2
+                {
+                    blocoComum(i, j);
+                }
+                if (i == 0 && j >= 0) // reta 3
+                {
+                    blocoComum(i, j);
+                }
+                if (j == 0 && i >= 0) // reta 4
+                {
+                    blocoComum(i, j);
+                }
+                if (i == 150 && j <= 0) // reta 5
+                {
+                    blocoComum(i, j);
+                }
+                if (j == -150) // reta 6
+                {
+                    blocoComum(i, j);
+                }
+                if (i == -30 && j == -150) {
+                    blocoInicio(i, j);
+                }
+            }
+        }
+
+    }
+
+
+    function blocoComum(x, z) {
+        var cubeGeometry = new THREE.BoxGeometry(50, 0.3, 50);
+        var cubeMaterial = new THREE.MeshBasicMaterial({ color: 'rgb(50,50,50)' });
+        var cube;
+
+        cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        cube.position.set(x, 0.15, z);
+        scene.add(cube);
+    }
+    function blocoInicio(x, z) {
+        var cubeGeometry = new THREE.BoxGeometry(50, 0.4, 50);
+        var cubeMaterial = new THREE.MeshBasicMaterial({ color: 'rgb(255, 0, 0)', });
+        var cube;
+
+        cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        cube.position.set(x, 0.15, z);
+
+        scene.add(cube);
+    }
+
+}
